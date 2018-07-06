a9c484dcf31cbf2d7878412e49ba34adff060e70
Edwin Zhao <edwin.zhao@outlook.com>
2018-07-06 14:16:20 -0400

    New Event Page fixed. Next implement event uploading to google

--- app/src/main/java/com/tomoed/ito/controller/NewEventFragment.java
+++ app/src/main/java/com/tomoed/ito/controller/NewEventFragment.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.tomoed.ito.R;
@@ -16,7 +17,10 @@ import static android.support.constraint.Constraints.TAG;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NewEventFragment extends Fragment implements View.OnClickListener{
+public class NewEventFragment extends Fragment {
+    private ImageButton quitFrag;
+    private Button submitEvent;
+
     public NewEventFragment() {
         // Required empty public constructor
     }
@@ -24,18 +28,31 @@ public class NewEventFragment extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_new_event, container, false);
 
-        return root;
-    }
+        quitFrag = root.findViewById(R.id.button_newEvent_close);
+        submitEvent = root.findViewById(R.id.button_new_event);
 
-    @Override
-    public void onClick(View v) {
-        int i = v.getId();
-        if (i == R.id.button_newEvent_close) {
-            Log.d(TAG, "PENSIDFHLSKDJFSDKFJLJS");
-            getActivity().getSupportFragmentManager().beginTransaction().remove(this).commit();
-        }
+        quitFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().beginTransaction()
+                        .remove(NewEventFragment.this).commit();
+            }
+        });
+
+        submitEvent.setOnClickListener(new View.OnClickListener() {
+
+            // TODO: Logic for implementing code for uploading onto Firebase here
+            @Override
+            public void onClick(View view) {
+
+
+                //Leave at bottom. Closes the fragment
+                getFragmentManager().beginTransaction().remove(NewEventFragment.this).commit();
+            }
+        });
+
+        return root;
     }
 }
